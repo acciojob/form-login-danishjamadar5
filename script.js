bb19dfad39b8ae146702954ba06cfd0329fd9854
danishjamadar5 <117918682+danishjamadar5@users.noreply.github.com>
2023-05-27 08:31:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+            var firstName = document.getElementsByName("fname")[0].value;
+            var lastName = document.getElementsByName("lname")[0].value;
+			// let firstName=form.["fname"].value;
+			// let lastName=form.["lname"].value;
 
-}
+			alert(firstName+" "+lastName);
+		}
